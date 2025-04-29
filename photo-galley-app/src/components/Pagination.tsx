b6ec52fcd50fe8f447213740ba0d75b77d406ed9
Jan Drabek <jasiekdrabek@gmail.com>
2025-04-29 14:28:55 +0200

    add more options of choose pages

--- photo-galley-app/src/components/Pagination.tsx
+++ photo-galley-app/src/components/Pagination.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { PaginationProps } from '../interfaces/PaginationProps';
 import './Pagination.css'
 
+const displayMorePages = ()=>{
+    for(let i=1; i <= 3; i++){
+
+    }
+}
 const Pagination: React.FC<PaginationProps> = ({ currentPage, setCurrentPage, maxPage }) => {
     return (
         <div className="pagination">
@@ -12,7 +17,13 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, setCurrentPage, ma
             >
                 Previous Page
             </button>
-            <span id="page-number">Page {currentPage}</span>
+            {[3,2,1].map(x=> (
+                currentPage-x>0 && <span onClick={() => setCurrentPage(currentPage - x)} id="page-number-3">{currentPage-x} </span>
+            ))}
+            <span onClick={() => setCurrentPage(currentPage)}id="page-number" className='current-page'>{currentPage}</span>
+            {[1,2,3].map(x=> (
+                currentPage+x<=maxPage && <span onClick={() => setCurrentPage(currentPage + x)} id="page-number-3">{currentPage+x} </span>
+            ))}
             <button
                 id="next-button"
                 onClick={() => setCurrentPage(currentPage + 1)}
